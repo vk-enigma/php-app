6f8fdcdc90c454f27656f604b76aad74b1760576
volter9 <volter925@gmail.com>
2015-05-05 12:25:08 -0700

    Update README.md

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
+/**
+ * @const string MF_BASEPATH Basepath of application
+ * @const string MF_APP_DIR  Application directory
+ * @const bool DEBUG Debug flag
+ */
 define('MF_BASEPATH', __DIR__ . '/');
 define('MF_APP_DIR' , __DIR__ . '/app/');
-
 define('DEBUG', true);
 
 require 'vendor/autoload.php';
